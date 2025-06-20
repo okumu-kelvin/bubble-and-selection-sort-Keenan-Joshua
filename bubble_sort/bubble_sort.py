e48f1a2526253346a3122820d9f5f7c55d13d398
Keenan <joshua.onyango@strathmore.edu>
2025-06-20 12:02:50 +0300

    Updated bubble_sort.py and selection_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    length = len(unsorted_list)
+    sorted_list = []
+    while len(sorted_list)!=length:
+        i = 0
+        while i < (len(unsorted_list) - 1):
+            if unsorted_list[i] < unsorted_list[i + 1]:
+                unsorted_list[i], unsorted_list[i + 1] = unsorted_list[i + 1], unsorted_list[i]
+            i = i + 1
+        sorted_list.append(unsorted_list[i])
+        unsorted_list.pop()
+
+    return sorted_list
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,16 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    length = len(arr)
+    sorted_arr=[]
+    while len(sorted_arr)!=length:
+        i = 0
+        min_value = arr[i]
+        x = i
+        while x < len(arr):
+            if min_value > arr[x]:
+                i = x
+                min_value = arr[i]
+            x = x + 1
+        arr[0], arr[i] = arr[i], arr[0]
+        sorted_arr.append(min_value)
+        arr.pop(0)
+    return sorted_arr
